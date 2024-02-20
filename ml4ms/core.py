c3fc20b7a7ba499cce8efaa149f831964413fc84
tinatn29 <tanaporn.nanarong@gmail.com>
2024-02-20 16:50:22 -0500

    add interpolation function - black

--- ml4ms/core.py
+++ ml4ms/core.py
@@ -4,12 +4,13 @@
 # from pathlib import Path
 # import re
 
-# import numpy as np
+import numpy as np
+
 # import pandas as pd
 # from pymatgen.analysis.bond_valence import BVAnalyzer
 # from pymatgen.analysis.local_env import CrystalNN
 # from pymatgen.core.structure import Structure
-# from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d
 
 """
 - Define class XANESCollection: list of dictionaries to store data
@@ -68,3 +69,39 @@ class XANESCollection:
         self.cn_dict = {}
         self.cs_dict = {}
         self.cs_mean_dict = {}
+
+    # Helper function to interpolate a XANES spectrum
+    def project_xanes_spectrum(self, x, y, proj_x=None, pad: bool = True):  # new x_domain
+        """
+        project (interpolate/extrapolate) a raw (x,y) xanes spectrum
+        onto the same grid (per element)
+
+        :param x: x-values (energy) from raw XANES spectra
+        :param y: y-values (mu) from raw XANES spectra
+        :param proj_x: new x_domains (array)
+        :param pad: Pad missing values with 0s. Otherwise extrapolates.
+
+        return projected xanes (y_new)
+        """
+        # interpolation function
+        # Pad with zeroes on the left of the original domain x, (set y_new = 0 where proj_x < min(x)
+
+        xmin = np.min(x)
+
+        if pad:
+            x_pad = np.array([proj_x[i] for i in range(len(proj_x)) if proj_x[i] < xmin])
+            y_pad = np.array([0.0] * len(x_pad))
+
+            # Return interpolation and extrapolate to right if necessary
+            func = interp1d(
+                np.concatenate((x_pad, x)),
+                np.concatenate((y_pad, y)),
+                kind="cubic",
+                fill_value="extrapolate",
+                assume_sorted=True,
+            )
+        else:
+            func = interp1d(x, y, kind="cubic", fill_value="extrapolate", assume_sorted=True)
+        # Occasionally the interpolation returns negative values
+        # with the left zero-padding, so manually ceil them to 0
+        return list(np.maximum(func(proj_x.round(8)), 0))
